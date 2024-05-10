72cf8e3254b791cc0c0795f6b97f4c0672ff8061
Rodri <rodrigoalvesdemacedo@live.com>
2024-05-11 01:35:51 +0200

    initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: "standard-with-typescript",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: ["./tsconfig.json"]
+  },
+  ignorePatterns: ["dist/"],
+  rules: {
+    "semi": "off",
+    "space-before-function-paren": "off",
+    "quotes": [2, "double"],
+    "quote-props": [2, "consistent"],
+    "multiline-ternary": "off",
+    "comma-dangle": "off",
+    "@typescript-eslint/quotes": [2, "double"],
+    "@typescript-eslint/semi": "off",
+    "@typescript-eslint/space-before-function-paren": "off"
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const text: string = "Hola amigos";
+console.log(text);
